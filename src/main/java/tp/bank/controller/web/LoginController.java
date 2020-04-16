177255a126825f3ed30eda42662f62f1c6870fef
Skalour <marceau.hollertt@hotmail.fr>
2020-04-16 19:08:05 +0200

    fix signup

--- src/main/java/tp/bank/controller/web/LoginController.java
+++ src/main/java/tp/bank/controller/web/LoginController.java
@@ -1,7 +1,6 @@
 package tp.bank.controller.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import tp.bank.model.Client;
+import tp.bank.model.Compte;
 import tp.bank.repository.ClientRepository;
+import tp.bank.repository.CompteRepository;
 
 @Controller
 public class LoginController {
@@ -21,6 +22,9 @@ public class LoginController {
 	
 	@Autowired
     private ClientRepository clientRepository;
+	
+	@Autowired
+	private CompteRepository compteRepository;
 	
 	@ModelAttribute("client")
 	    public Client userRegistrationDto() {
@@ -35,17 +39,6 @@ public class LoginController {
     @PostMapping("/signup")
     public String registerUserAccount(@ModelAttribute("client")Client client,
                                       BindingResult result){
-
-        //Client existing = userService.findByEmail(userDto.getEmail());
-
-       /* if (existing != null){
-            result.rejectValue("email", null, "Cet e-mail est déjà utilisé.");
-        }
-
-        if (result.hasErrors()){
-            return "signup";
-        }*/
-
         this.save(client);
         return "redirect:/login";
     }
@@ -58,18 +51,22 @@ public class LoginController {
 	}
 
 	public Client save(Client newClient){
-	     Client client = new Client();
-	        client.setLastname(newClient.getLastname());
-	        client.setFirstname(newClient.getFirstname());
-	        client.setPostalAddress(newClient.getPostalAddress());
-	        client.setPostalCode(newClient.getPostalCode());
-	        client.setCity(newClient.getCity());
-	        client.setBirthdate(newClient.getBirthdate());
-	        client.setPhoneNumber(newClient.getPhoneNumber());
-	        client.setEmail(newClient.getEmail());
-	        client.setPassword(passwordEncoder.encode(newClient.getPassword()));
-	        client = clientRepository.save(client);
-	        return client;
+	    Client client = new Client();
+	    Compte compte = new Compte();
+        client.setLastname(newClient.getLastname());
+        client.setFirstname(newClient.getFirstname());
+        client.setPostalAddress(newClient.getPostalAddress());
+        client.setPostalCode(newClient.getPostalCode());
+        client.setCity(newClient.getCity());
+        client.setBirthdate(newClient.getBirthdate());
+        client.setPhoneNumber(newClient.getPhoneNumber());
+        client.setEmail(newClient.getEmail());
+        client.setPassword(passwordEncoder.encode(newClient.getPassword()));
+        client = clientRepository.save(client);
+        compte.setBalance(1000.00);
+        compte.setClient(client);
+        compteRepository.save(compte);
+        return client;
 	    }
 
 }
